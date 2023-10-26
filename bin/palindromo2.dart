4e36a0cbfcc2adaaf653bad7ee3dead6cbcf5d9b
jonathanmaker <jonathantbm2015@gmail.com>
2023-10-25 23:48:02 -0300

    correccion de importacion y pubspec.yaml

--- bin/palindromo2.dart
+++ bin/palindromo2.dart
@@ -1,4 +1,7 @@
-import '' as palindromo2; //Lo tengo vacio por que no me toma mi package
+import '../lib/verificar_palindromo.dart'
+    as palindromo2; //Lo tengo vacio por que no me toma mi package
 
 void main(List<String> arguments) {
+  print(
+      'verificador de palindromo arrojo : ${palindromo2.verificarPalindromo('el ricardo se la come')}!'); // reemplazar en el mensaje interpolado verificar palindromo la palabra a verificar por la que se desee
 }
